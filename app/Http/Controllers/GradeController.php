8e8e84e4b12d361083323245bc44be8a3202b638
Boris Assoi <boris.assoi@outlook.com>
2021-02-05 19:04:33 +0000

    Ajout de Model et du Controller de grade

---
+++ app/Http/Controllers/GradeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Grade;
+
+class GradeController extends Controller
+{
+    /**
+     * Return all grades 
+     * 
+     * @return User
+     */
+    public function showAll()
+    {
+        return response()->json(Grade::all());
+    }
+
+    /**
+     * Return one 
+     * 
+     * @param $id Identifiant du grade
+     * @return User
+     */
+    public function showOneUser($id)
+    {
+        return response()->json(Grade::find($id));
+    }
+}
