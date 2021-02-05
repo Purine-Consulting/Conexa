f78852484a016f9e304a50a1ac11f9960fe8e242
Boris Assoi <boris.assoi@outlook.com>
2021-02-05 19:34:24 +0000

    Fonctionnalité d'ajout du Mentor

---
+++ app/Mentor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+class Mentor extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'mentor';
+}
