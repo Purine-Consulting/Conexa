5cde8a4619a4274d3823afeaa0eedec06bbfb3db
Boris Assoi <boris.assoi@outlook.com>
2021-02-05 14:21:09 +0000

    Ajout du champ du nom de la table

--- app/User.php
+++ app/User.php
@@ -14,6 +14,13 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     use Authenticatable, Authorizable;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
