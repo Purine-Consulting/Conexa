8ac6754d633a75af0de9d40f5ca5c21080e883c3
Boris Assoi <boris.assoi@outlook.com>
2021-02-19 15:21:36 +0000

    Ajout de la route de logout

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->group(['prefix' => 'auth'], function () use ($router) {
         $router->post('login', 'AuthController@login');
         $router->post('register', 'AuthController@register');
+        $router->post('logout', 'AuthController@logout');
     });
 
     $router->group(['middleware' => 'auth:api'], function () use ($router) {
